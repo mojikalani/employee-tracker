e11f7f8239d76b085fde818593bc2ebe3915cf94
kalanimojica <mojica.kalani@gmail.com>
2021-10-25 11:15:18 -0700

    Starter server js code:

---
+++ server.js
@@ -0,0 +1,28 @@
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+const consoleTable = require('console.table')
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      user: 'root',
+      password: 'password',
+      database: 'employee_tracker_db'
+    }
+  );
+
+  // Default response for any other request (Not Found)
+app.use((req, res) => {
+    res.status(404).end();
+  });
+  
+  app.listen(PORT, () => {
+    console.log(`Server listening on  http://localhost${PORT}`);
+  });
